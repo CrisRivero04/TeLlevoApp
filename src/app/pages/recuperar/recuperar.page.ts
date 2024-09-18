49440f0388d060b2de3e936be0c40aa70c9d76df
Cristobal Rivero <cr.jarar@duocuc.cl>
2024-09-18 15:08:37 -0300

    Creacion de readme, recuperar contraseña terminado y cosas nuevas en registro

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-recuperar',
@@ -7,20 +9,45 @@ import { Router } from '@angular/router';
   styleUrls: ['./recuperar.page.scss'],
 })
 export class RecuperarPage implements OnInit {
-  titulo: string = "Restablecer Contraseña";
-
-  constructor(private router: Router) { }
+  
+  constructor(private alertController: AlertController, private navCtrl: NavController) { }
 
   ngOnInit() {
   }
 
   //NgModel
-  password: string = "";
-  repetir_password: string = "";
+  correo: string = "";
+  correoValido: boolean = true;
+  titulo: string = "Restablecer Contraseña";
 
-  recuperar() {
-    // Aquí puedes agregar la lógica de autenticación
-    console.log('Iniciando sesión...');
-    this.router.navigate(['/login']); // Navega a la página principal tras el login
+  async recuperar(formulario: any) {
+    if (formulario.valid && this.correoValido) {
+      const alert = await this.alertController.create({
+        header: 'Solicitud Enviada',
+        message: 'Se ha enviado al correo la solicitud de cambio de contraseña.',
+        buttons: [
+          {
+            text: 'OK',
+            handler: () => {
+              // Navegar a la página de login
+              this.navCtrl.navigateRoot('/login');
+            }
+          }
+        ]
+      });
+      await alert.present();
+    } else {
+      // Si el correo no es válido o no sigue el formato
+      if (!this.correoValido) {
+        console.log('El correo no sigue el formato correcto.');
+      }
+    }
   }
+
+  validarCorreo(correo: string) {
+    // Expresión regular para validar que el correo termine en @duocuc.cl
+    const regex = /^[a-zA-Z0-9._%+-]+@duocuc\.cl$/;
+    this.correoValido = regex.test(correo);
+  }
+
 }
--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -13,13 +13,17 @@ export class RegistroPage implements OnInit {
 
   persona = new FormGroup({
     rut: new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required,Validators.pattern("[0-9]{7,8}-[0-9kK]{1}")]),
-    p_nombre: new FormControl('',[Validators.minLength(2),Validators.required,Validators.pattern("[a-z]{2,25}")]),
-    s_nombre: new FormControl('',[Validators.minLength(2),Validators.pattern("[a-z]{2,25}")]),
-    ap_paterno: new FormControl('',[Validators.minLength(2),Validators.required,Validators.pattern("[a-z]{2,25}")]),
-    ap_materno: new FormControl('',[Validators.minLength(2),Validators.required,Validators.pattern("[a-z]{2,25}")]),
+    correo: new FormControl('',[Validators.required]),
+    nombre: new FormControl('',[Validators.minLength(2),Validators.required,Validators.pattern("[a-z]{2,25}")]),
+    apellido: new FormControl('',[Validators.minLength(2),Validators.required,Validators.pattern("[a-z]{2,25}")]),
     contraseña: new FormControl('',[Validators.required]),
+    rep_contraseña: new FormControl('',[Validators.required]),
     fecha_nacimiento: new FormControl('',[Validators.required]),
-    genero: new FormControl('',[Validators.required]),
+    tiene_vehiculo: new FormControl('no',[Validators.required]),
+    marca_vehiculo: new FormControl('',[Validators.required,Validators.pattern("[a-z][A-Z]")]),
+    modelo_vehiculo: new FormControl('',[Validators.required,Validators.pattern("[a-z][A-Z]")]),
+    patente: new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(6),Validators.pattern("[a-z][A-Z][0-9]")]),
+    anio_inscripcion: new FormControl('',[Validators.required,Validators.pattern("{1900, 2024}")])
   });
 
   constructor(private router: Router) { }
