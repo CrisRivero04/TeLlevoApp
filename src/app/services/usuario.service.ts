366c28362d78b5ea4126223957fe855b89ef8a56
Jotita <je.padilla@duocuc.cl>
2024-09-21 22:12:40 -0300

    agregar obtener usuario services

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -4,6 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UsuarioService {
+  //aqui van las variables
+  usuarios: any[] = [
+
+    
+  ];
 
   constructor() { }
+
+  //aqui va la lógica(crud)
+
+  public obtenerUsuario(rut:string):any{
+    return this.usuarios.find(usuario => usuario.rut == rut);
+  }
+
+  public obtenerUsuarios(): any[]{
+    return this.usuarios;
+  }
 }
