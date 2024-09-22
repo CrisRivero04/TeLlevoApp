937e6646d6d69c800acd7cd1f8be4c3c332b9af1
Jotita <je.padilla@duocuc.cl>
2024-09-21 22:38:15 -0300

    creacion updateUser en services

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -22,6 +22,15 @@ export class UsuarioService {
     return false;
   }
 
+  public updateUser(rut:string, newUser:any){
+    const indice = this.usuarios.findIndex(usuario => usuario.rut == rut);
+    if(indice == -1){
+      return false;
+    }
+    this.usuarios[indice] = newUser;
+    return true;
+  }
+
   public obtenerUsuario(rut:string):any{
     return this.usuarios.find(usuario => usuario.rut == rut);
   }
